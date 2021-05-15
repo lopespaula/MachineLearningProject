1b87ba70ea6b47fc4ccc51629b9fae9d5b16ca6d
Your Name <you@example.com>
2021-05-15 04:54:56 -0700

    .sh fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
-" > ~/.streamlit/config.toml
+# mkdir -p ~/.streamlit/                                               
+# echo "\                       
+# [server]\n\                       
+# port = $PORT\n\                       
+# enableCORS = false\n\                       
+# headless = true\n\                       
+# \n\                       
+# " > ~/.streamlit/config.toml
+
+mkdir -p ~/.streamlit/
+ echo “
+ [server]
+ headless = true
+ enableCORS=false
+ port = $PORT
+ “ > ~/.streamlit/config.toml
